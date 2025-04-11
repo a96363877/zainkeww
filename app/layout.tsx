a1c44b614a22792dfe9d0a1bb78a083ca2b93556
a96363877 <a96363877@gmail.com>
2025-04-11 21:25:19 +0800

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+  </head>
       <body>
           {children}
         <Script src={`https://www.googletagmanager.com/gtag/js?id=AW-410329319`} strategy="afterInteractive" />
